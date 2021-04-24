686853dff6accdfe5199239bcc7b3ba0099ffc4e
Laudair Egevarth <laudair.egevarth@hotmail.com>
2021-04-24 21:46:29 +1000

    build(feature):introduce screens

--- App.js
+++ App.js
@@ -1,65 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View, StatusBar} from 'react-native';
 import Home from './src/pages/home';
+import Routes from './src/pages/routes';
 
 import axios from 'axios';
 
 const App = () => {
-  const [news, setNews] = useState([]);
+  // const [news, setNews] = useState([]);
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b87051b16e404e1bb92f0d59380c310d',
-      );
-      /**
-       * status: 'ok',
-      totalResults: 16195
-      se precisar eh so pegar antes do ressponse.data (response.data.status e .totalResults)
-       */
-      // console.log('response', response.data.articles);
-      setNews(response.data.articles);
-    };
-    fetchNews();
-  }, []);
+  // useEffect(() => {
+  //   const fetchNews = async () => {
+  //     const response = await axios.get(
+  //       'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b87051b16e404e1bb92f0d59380c310d',
+  //     );
+  //     setNews(response.data.articles);
+  //   };
+  //   fetchNews();
+  // }, []);
 
   return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: 70,
-      }}>
-      <Home news={news} />
-    </View>
+    // <View
+    //   style={{
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     paddingTop: 70,
+    //   }}>
+    //   <Home news={news} />
+    // </View>
+    <Routes />
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import {View, Image, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-const Card = ({title, image, date, author}) => {
-  let day = new Date();
-  let today =
-    day.getFullYear() +
-    '-' +
-    '0' +
-    parseInt(day.getMonth() + 1) +
-    '-' +
-    day.getDate();
-
-  let yesterday =
-    day.getFullYear() +
-    '-' +
-    '0' +
-    parseInt(day.getMonth() + 1) +
-    '-' +
-    (day.getDate() - 1);
+const Card = ({title, image, date, author, url, content}) => {
+  const navigation = useNavigation();
 
   return (
     <View style={{padding: 4, display: 'flex', flexDirection: 'row'}}>
       <View style={{display: 'flex', flexDirection: 'column'}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold', width: 268}}>
+        <Text
+          style={{fontSize: 18, fontWeight: 'bold', width: 268}}
+          accessibilityRole="button"
+          accessible={true}
+          onPress={() =>
+            navigation.navigate('Details', {
+              title: title,
+              image: image,
+              date: date,
+              content: content,
+              author: author,
+              url: url,
+            })
+          }>
           {title.length < 85 ? `${title}` : `${title.substring(0, 85)}...`}
           {/* {title} */}
         </Text>
         <Text>{author}</Text>
-        <Text>
-          {date.substring(0, 10) === today
-            ? 'Today'
-            : date.substring(0, 10) === yesterday
-            ? 'Yesterday'
-            : `${date.substring(0, 10)}`}
-        </Text>
+        <Text>{date}</Text>
       </View>
       <Image
         style={{width: 106, height: 118, borderRadius: 4}}
