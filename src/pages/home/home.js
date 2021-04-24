61741ae9bd00eca9a9b74e477511c3c83d03f261
Laudair Egevarth <laudair.egevarth@hotmail.com>
2021-04-24 22:19:35 +1000

    build(feature): introduce refresh on scroll

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View, ScrollView} from 'react-native';
-import NewsCard from '../../components/card';
+import React, {useState, useCallback, useEffect} from 'react';
+import {Text, View, ScrollView, RefreshControl} from 'react-native';
+import Card from '../../components/card';
+
+import axios from 'axios';
 
 const API_KEY = 'b87051b16e404e1bb92f0d59380c310d';
 
-const Home = ({news, navigation}) => {
+const wait = timeout => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
+
+const Home = ({navigation}) => {
+  const [refreshing, setRefreshing] = useState(false);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b87051b16e404e1bb92f0d59380c310d',
+      );
+      setNews(response.data.articles);
+    };
+    fetchNews();
+  }, [refreshing]);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   let day = new Date();
   let today =
     day.getFullYear() +
@@ -23,15 +47,19 @@ const Home = ({news, navigation}) => {
     (day.getDate() - 1);
 
   return (
-    <ScrollView style={{backgroundColor: '#fff'}}>
+    <ScrollView
+      style={{backgroundColor: '#fff'}}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
       <Text style={{fontWeight: 'bold', fontSize: 28, padding: 4}}>NEWS</Text>
       <Text>
-        {news.map((article, idx) => {
+        {news.map(article => {
           return (
             <>
-              <NewsCard
+              <Card
                 title={article.title}
-                key={idx}
+                key={article.title}
                 image={article.urlToImage}
                 date={
                   article.publishedAt.substring(0, 10) === today
--- src/pages/routes/routes.js
+++ src/pages/routes/routes.js
@@ -9,23 +9,22 @@ import Details from './../details';
 const Stack = createStackNavigator();
 
 const Routes = () => {
-  const [news, setNews] = useState([]);
+  // const [news, setNews] = useState([]);
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b87051b16e404e1bb92f0d59380c310d',
-      );
-      setNews(response.data.articles);
-    };
-    fetchNews();
-  }, []);
+  // useEffect(() => {
+  //   const fetchNews = async () => {
+  //     const response = await axios.get(
+  //       'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b87051b16e404e1bb92f0d59380c310d',
+  //     );
+  //     setNews(response.data.articles);
+  //   };
+  //   fetchNews();
+  // }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" options={{title: 'News'}}>
-          {props => <Home news={news} />}
-        </Stack.Screen>
+        <Stack.Screen name="Home" component={Home} options={{title: 'News'}} />
+
         <Stack.Screen
           name="Details"
           component={Details}
